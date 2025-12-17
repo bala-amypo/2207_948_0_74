3522ba1fbf2560e565358de025e71dafd0978ca7
bala-amypo <bala@amypo.in>
2025-12-17 08:44:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/studentEntity.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+
+import jarkata.persistence.*;
+import jarkata.validation.constraints.*;
+
+@Entity 
+public class studentEntity {
+    @Id
+    @GeneratedValue(stratergy =GenerationType.IDENTITY)
+    private Lond id;
+
+    @NotBlank(message=" Name is not Valid");
+    private String name;
+
+    @Email(message="Email Format is not Valid");
+    private String email;
+
+    public void setId(Long id){
+        this.id =id ;
+
+    }
+    public Long getId(){
+        return id;
+    }
+    public studentEntity(){
+        
+    }
+        public studentEntity(Long id,String name,String email){
+            this.id=id;
+            this.name=name;
+            this.email=email;
+        }
+    
+}
