e8e45f6ffc42969c5f8984f9edb6143db981f827
bala-amypo <bala@amypo.in>
2025-12-17 09:28:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/studentEntity.java
+++ src/main/java/com/example/demo/entity/studentEntity.java
@@ -1,33 +1,53 @@
 package com.example.demo.entity;
 
-import jarkata.persistence.*;
-import jarkata.validation.constraints.*;
-@Entity 
-public class studentEntity {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table(name = "students")
+public class StudentEntity {
+
     @Id
-    @GeneratedValue(stratergy =GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message=" Name is not Valid")
+    @NotBlank(message = "Name is not valid")
     private String name;
 
-    @Email(message="Email Format is not Valid")
+    @Email(message = "Email format is not valid")
     private String email;
 
-    public void setId(Long id){
-        this.id =id ;
-
+    public StudentEntity() {
     }
-    public Long getId(){
+
+    public StudentEntity(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters & Setters
+    public Long getId() {
         return id;
     }
-    public studentEntity(){
 
+    public void setId(Long id) {
+        this.id = id;
     }
-        public studentEntity(Long id,String name,String email){
-            this.id=id;
-            this.name=name;
-            this.email=email;
-        }
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
